0a2063a7687e3cd079e58f009d3a4caeb86f8059
Masaya Yoshino <masayaliketennis@yahoo.co.jp>
2017-09-04 09:41:12 +0000

    seedデータを更新

--- db/seeds.rb
+++ db/seeds.rb
@@ -9,3 +9,29 @@
 # coding:utf-8
 
 Song.create(:song_id => '1', :song_name => 'sorry', :album_id => '1', :artist_id => '1', :lyrics_data => 'sample', :youtube_id => 'sample', :youtube_start => '50')
+Song.create(
+    :song_id => '2', 
+    :song_name => 'sample', 
+    :album_id => '2', 
+    :artist_id => '2', 
+    :lyrics_data => '{"lines": [
+{
+"time": 24540, 
+"words": [
+{
+"end": 26140, 
+"start": 24540, 
+"string": "ざわめい"
+}, 
+{
+"end": 26600, 
+"start": 26170, 
+"string": "た"
+}, 
+{
+"end": 27530, 
+"start": 26630, 
+"string": "街"
+}]}', 
+    :youtube_id => 'sample', 
+    :youtube_start => '50')
